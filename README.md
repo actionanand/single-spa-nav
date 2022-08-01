e73a861c9ae18378e12efb46f384973759f0a40f
Anand raja <meetanandns@gmail.com>
2022-08-01 19:05:36 +0000

    working nav bar

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 
 ## Overview
 
-This repo is used in conjunction with Seven other repos listed below. Together they make up an application composed of microfrontends. Each app can be updated and deployed independently from the others.
+This repo is used in conjunction with eight other repos listed below. Together they make up an application composed of microfrontends. Each app can be updated and deployed independently from the others.
 
 - [**Root Config**](https://github.com/actionanand/single-spa-demo-root-config)
 - [**Angular App**](https://github.com/actionanand/single-spa-angular)
@@ -15,6 +15,7 @@ This repo is used in conjunction with Seven other repos listed below. Together t
 - [**Nav Bar App** (This Repo)](https://github.com/actionanand/single-spa-nav)
 - [**Footer App**](https://github.com/actionanand/single-spa-footer)
 - [**404 App**](https://github.com/actionanand/single-spa-404)
+- [**Vanilla JS App** (This Repo)](https://github.com/actionanand/single-spa-vanilla-js)
 
 ## Demo
 
--- src/root.component.js
+++ src/root.component.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import { BrowserRouter, Link } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+
 import classes from './root.component.module.css';
+
+import NavigationItem from './NavigationItem/NavigationItem';
 
 export default function Root() {
   return (
     <BrowserRouter>
       <nav className={classes.nav}>
-        <Link to="/" className={classes.link}>
-          Home
-        </Link>
-        <Link to="/angular" className={classes.link}>
-          Angular
-        </Link>
-        <Link to="/react" className={classes.link}>
-          React
-        </Link>
-        <Link to="/vue" className={classes.link}>
-          Vue
-        </Link>
-        <Link to="/svelte" className={classes.link}>
-          Svelte
-        </Link>
+        <NavigationItem link="/">Home</NavigationItem>
+        <NavigationItem link="angular">Angular</NavigationItem>
+        <NavigationItem link="react">React</NavigationItem>
+        <NavigationItem link="vue">Vue</NavigationItem>
+        <NavigationItem link="svelte">Svelte</NavigationItem>
       </nav>
     </BrowserRouter>
   );
